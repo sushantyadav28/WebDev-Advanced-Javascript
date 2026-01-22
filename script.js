be2ea175ec5eb828d86775cae09c6930d4837a55
Sushant Yadav <sy7344678@gmail.com>
2026-01-22 14:38:52 +0530

    first code

---
+++ script.js
@@ -0,0 +1,22 @@
+// const para=document.getElementsByClassName("para")       //Returns HTMLCollection
+// para[0].textContent="This is updated one"
+// para[1].textContent="This is updated two"
+// console.log(para)
+
+
+
+// const para=document.querySelectorAll(".para")       //Returns NodeList
+// para[0].textContent="This is updated one"
+// para[1].style.color="green"
+// console.log(para)
+
+
+
+// const container=document.querySelector(".container")
+// container.innerHTML="<h1>This is a heading</h1>"
+// console.log(container)
+
+
+
+const button=document.querySelector("button")
+button.classList.add("btn")
